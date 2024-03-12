c3c9cb67e0164d88725fbc60e16c167a90b533a4
y-takagiiii <takagiyuri0712@gmail.com>
2024-03-12 18:09:00 +0900

    canvasを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="w-full h-screen">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial color="red"/>
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
