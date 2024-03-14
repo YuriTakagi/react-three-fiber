4f31839cb826fad7f5978c2ce2135072ca703056
y-takagiiii <takagiyuri0712@gmail.com>
2024-03-14 10:32:07 +0900

    各コンポーネントを読み込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import "./App.css";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
+import SelectBox from "./components/SelectBox";
+import BasicBox01 from "./components/BasicBox01";
+import BasicBox02 from "./components/BasicBox02";
+import ControlBox01 from "./components/ControllBox";
 
 function App() {
+  const pullDownOptions = [1, 2, 3, 4, 5];
+  const [selectedOption, setSelectedOption] = useState(pullDownOptions[0]);
+
   return (
     <div className="w-full h-screen">
+      <div className="p-2">
+        <SelectBox
+          options={pullDownOptions}
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+        />
+      </div>
+      {/* Canvasでr3fの定義を始める */}
       <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial color="red"/>
-        </mesh>
+        {selectedOption === 1 && <BasicBox01 />}
+        {selectedOption === 2 && <BasicBox02 />}
+        {selectedOption === 3 && <ControlBox01 />}
       </Canvas>
     </div>
   );
